2d802c64a1660bbe40d52236dd4e99fc827dcf07
Steven Yu <Stevenyu@mail.weber.edu>
2020-11-03 15:51:58 -0700

    'commit'

--- cms/admin/auth.py
+++ cms/admin/auth.py
@@ -1,4 +1,10 @@
 from cms.admin import admin_bp
 from cms.admin.models import User
 from flask import render_template, request, redirect, url_for, flash
+from functools import wraps
+from flask import session, g
+
+def protected(route_function):
+    def wrapped_route_function(**kwargs):
+        return route_function(**kwargs)
 
