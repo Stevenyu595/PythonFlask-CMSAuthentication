effdf3af2604b70646f5af94399e93ea0e833a31
Steven Yu <Stevenyu@mail.weber.edu>
2020-11-03 21:49:40 -0700

    'commit'

--- cms/admin/auth.py
+++ cms/admin/auth.py
@@ -16,3 +16,30 @@ def protected(route_function):
 def load_user():
     user_id = session.get('user_id')
     g.user = User.query.get(user_id) if user_id is not None else None
+
+@admin_bp.route('/login', methods=('GET', 'POST'))
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        error = None
+
+        user = User.query.filter_by(username=username).first()
+
+        if user is None:
+            error = 'Incorrect username.'
+        elif not user.check_password(password):
+            error = 'Incorrect password'
+
+        if error is None:
+            session.clear()
+            session['user_id'] = user.id
+            return redirect(url_for('admin.content', type='page'))
+
+        flash(error)
+    return render_template('admin/login.html')
+
+@admin_bp.route('/logout')
+def logout():
+    session.clear()
+    return redirect(url_for('admin.login'))
