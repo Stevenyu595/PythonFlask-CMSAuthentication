ad54b1b5bfdb65f6d120d2869344fbacd07a4231
Steven Yu <Stevenyu@mail.weber.edu>
2020-11-03 21:17:26 -0700

    'commit'

--- cms/admin/auth.py
+++ cms/admin/auth.py
@@ -7,6 +7,6 @@ from flask import session, g
 def protected(route_function):
     def wrapped_route_function(**kwargs):
         if g.user is None:
-            redirect(url_for('admin.login'))
+            return redirect(url_for('admin.login'))
         return route_function(**kwargs)
 
